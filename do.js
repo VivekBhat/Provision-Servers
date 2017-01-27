6c3ec38f3edd1c3297e83eb5a2188a6c6f3b0442
Vivek Bhat <vivekbhat@live.com>
2017-01-26 19:34:56 -0500

    digital ocean droplet creation file added

--- do.js
+++ do.js
@@ -2,45 +2,28 @@ var needle = require("needle");
 var os   = require("os");
 var exec = require('child_process').exec;
 var sys = require('sys');
+var sleep = require("sleep");
 
 // var config = {};
 var config_token = process.env.DO;
+console.log();
+console.log("============ DigitalOcean Droplet ============");
 
-console.log(config_token);
 var headers =
 {
 	'Content-Type':'application/json',
 	Authorization: 'Bearer ' + config_token
 };
 
-// Documentation for needle:
-// https://github.com/tomas/needle
-
 var client =
 {
-	listRegions: function( onResponse )
-	{
-		needle.get("https://api.digitalocean.com/v2/regions", {headers:headers}, onResponse)
-	},
-	listImages: function( onResponse )
-	{
-		needle.get("https://api.digitalocean.com/v2/images", {headers:headers}, onResponse)
-	},
-	
-	listIp: function( onResponse )
-	{
-		needle.get("https://api.digitalocean.com/v2/droplets/38162969", {headers:headers}, onResponse)
-	},
-	destroyIP: function (onResponse){
-	var data = 
-	{
-	
-	"ssh_keys":["KEY HERE"],
 
-	};
-	
-		needle.delete("https://api.digitalocean.com/v2/droplets/38162969",data, {headers:headers,json:true}, onResponse)
+	listIp: function(dropletId, onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/droplets/" + dropletId, {headers:headers, json:true}, onResponse);
 	},
+// 38321153
+
 	createDroplet: function (dropletName, region, imageName, onResponse)
 	{
 		var data = 
@@ -65,140 +48,26 @@ var client =
 	}
 	// getIP: function()
 };
-
-// #############################################
-// #1 Print out a list of available regions
-// Comment out when completed.
-// https://developers.digitalocean.com/documentation/v2/#list-all-regions
-// use 'slug' property
-client.listRegions(function(error, response)
+var dropletId = 0;
+var name = "HW1";
+var region = "nyc2";
+var image = "ubuntu-14-04-x64";
+client.createDroplet(name, region, image , function(err, resp, body)
 {
-	var data = response.body;
-	//console.log( JSON.stringify(response.body) );
-
-	if( response.headers )
-	{
-		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
-	}
-// regions
-	if( data.regions )
-	{
-		for(var i=0; i<data.regions.length; i++)
-		{
-			// console.log(data.regions[i].name);
-		}
-	}
-});
-
-client.listImages(function(error, response)
-{
-	var data = response.body;
-	//console.log( JSON.stringify(response.body) );
+	console.log("============ Creating DigitalOcean Droplet ============");	
+	if(!err &&resp.statusCode==202){
+	console.log("Droplet created");
+	dropletId = body.droplet.id;
+	console.log("Droplet ID is: ", dropletId);
+	console.log("Wait for 30 seconds to retrieve IP");
 	console.log();
-	if( response.headers )
-	{
-		console.log( "Calls remaining", response.headers["ratelimit-remaining"] );
-	}
-// regions
-	if( data.images )
-	{
-		for(var i=0; i<data.images.length; i++)
-		{
-		// console.log(data.images);
-		}
+
+	console.log("###### Getting the IP Address ######");
+	sleep.sleep(30);
+	
+	client.listIp(dropletId, function(err,resp,body){
+		console.log("IP Address is: ",body.droplet.networks.v4[0].ip_address);
+		});	
 	}
 });
-
-
-// #############################################
-// #2 Extend the client object to have a listImages method
-// Comment out when completed.
-// https://developers.digitalocean.com/documentation/v2/#images
-// - Print out a list of available system images, that are AVAILABLE in a specified region.
-// - use 'slug' property
-
-// #############################################
-// #3 Create an droplet with the specified name, region, and image
-// Comment out when completed. ONLY RUN ONCE!!!!!
-// Write down/copy droplet id.
-// var name = "UnityId"+os.hostname();
-// var region = ""; // Fill one in from #1
-// var image = ""; // Fill one in from #2
-var dropletId = "6085003";
-
-
-client.createDroplet('Testing1', 'nyc2', 'ubuntu-14-04-x64', function(err, resp)
-{
-	// console.log(body);
-	console.log("###################");
-	console.log("Is there any error ?");
-
-	console.log(JSON.stringify(err));
-	console.log("###################");
-	console.log( JSON.stringify( resp.body ) );
-	console.log("###################");	
-
-	// var dropletId = resp.body.droplet.id;
-	console.log("dropletId inside is: ");
-	console.log(dropletId);
-
-
-});
-
-	// client.listIp(function(error, response){
-	
-	// //console.log(response);
-	// var data = response.body;
-	// console.log( JSON.stringify(response.body) );
-	// ip = response.body.droplet.networks.v4[0].ip_address;
-	// console.log();
-	// console.log("########--DISPLAYING IP---##########");
-	// console.log(response.body.droplet.networks.v4[0].ip_address);
-	// console.log();
-	// console.log("Pinging IP");
-	// function puts(error, stdout, stderr) { sys.puts(stdout) }
-	// exec("ping -c 4 "+response.body.droplet.networks.v4[0].ip_address, puts);
-	// console.log();
-	// });
-
-// #############################################
-// #4 Extend the client to retrieve information about a specified droplet.
-// Comment out when done.
-// https://developers.digitalocean.com/documentation/v2/#retrieve-an-existing-droplet-by-id
-// REMEMBER POST != GET
-// Most importantly, print out IP address!
-console.log("dropletId outside is: ");
-console.log(dropletId);
-
-
-// #############################################
-// #5 In the command line, ping your server, make sure it is alive!
-// ping xx.xx.xx.xx
-
-// #############################################
-// #6 Extend the client to DESTROY the specified droplet.
-// Comment out when done.
-// https://developers.digitalocean.com/documentation/v2/#delete-a-droplet
-// HINT, use the DELETE verb.
-// HINT #2, needle.delete(url, data, options, callback), data needs passed as null.
-// No response body will be sent back, but the response code will indicate success.
-// Specifically, the response code will be a 204, which means that the action was successful with no returned body data.
-
-
-// client.destroyIP(function (err,resp) {
-// console.log("Deleting!");
-// if(!err && resp.statusCode == 204)
-// 	{
-// 			console.log("Deleted!");
-// 	}
-
-// 	exec("ping -c 4 "+resp.body.droplet.networks.v4[0].ip_address, puts);
-
-// });
-	
-
-// #############################################
-// #7 In the command line, ping your server, make sure it is dead!
-// ping xx.xx.xx.xx
-// It could be possible that digitalocean reallocated your IP address to another server, so don't fret it is still pinging.
 
